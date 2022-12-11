7722e46ae39e43ee3d6391835bf05f4fdc924cb0
Ramazant22 <120320309+Ramazant22@users.noreply.github.com>
2022-12-11 18:10:29 +0300

    yazılım mühendisliği oryantasyon sınav
    https://www.youtube.com/watch?v=7ihDr5-bST4

---
+++ yazilimoryantasyon/src/yazilimoryantasyon/Yazilimoryantasyon.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package yazilimoryantasyon;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ramazan
+ */
+public class Yazilimoryantasyon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+     /*   Scanner scan=new Scanner(System.in);
+        System.out.println("lütfen bir sayı giriniz");
+    int sayi=scan.nextInt();
+        if (sayi%3==0 && sayi%5==0) {
+            System.out.println("girdiğiniz sayı 3 ve 5 in katıdır");
+        }else 
+            System.out.println("girdiğiniz sayı 3 ve 5 in katı değildir");
+    
+        */
+        
+   /*      int toplam=0;
+   Scanner scan=new Scanner(System.in);
+        System.out.println("lütfen bir sayı giriniz");
+        int sayi=scan.nextInt();
+        for (int i = 0; i <=sayi; i++) {
+            toplam+=sayi;
+            
+        }
+       
+        System.out.println(toplam);
+        */
+   
+   /* 
+    Scanner scan=new Scanner(System.in);
+        System.out.println("lütfen bir sayı giriniz");  
+    int sayi=scan.nextInt();
+        if (sayi%3==0) {
+            System.out.println("girdiğiniz sayı 3 ün katıdır");
+        }else 
+            System.out.println("girdiğiniz sayı 3 ün katı değildir");
+    */
+   
+   /* Scanner scan=new Scanner(System.in);
+        System.out.println("lütfen 1. sayıyı giriniz");
+    int sayi1=scan.nextInt();
+     System.out.println("lütfen 2. sayıyı giriniz");
+    int sayi2=scan.nextInt();
+        if (sayi1>sayi2) {
+            System.out.println("1. sayı 2.sayıdan büyüktür");
+        }
+    
+        else if(sayi2>sayi1)
+        {
+            System.out.println("2. sayı 1.sayıdan büyüktür");
+        }else 
+            System.out.println("iki sayı birbirine");
+   */
+   
+   /*int toplam=0;  
+    Scanner scan=new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("lütfen "+(i+1)+". sayıyı giriniz");
+            int sayi=scan.nextInt();
+            toplam+=sayi;
+        }
+        System.out.println("girdiğiniz sayıların ortalaması"+toplam/5);
+    
+    *//*Scanner scan=new Scanner(System.in);
+        System.out.println("lütfen ürünün tutarını giriniz ");
+    int tutar=scan.nextInt();
+    int kdv=(tutar*18)/100;
+        System.out.println("ürünün kdv li fiyatı "+(kdv+tutar));
+   */ 
+    
+   /* Scanner scan=new Scanner(System.in);
+        System.out.println("lütfen kısa kenarı giriniz");
+    int kisak=scan.nextInt();
+    System.out.println("lütfen uzun kenarı giriniz");
+    int uzunk=scan.nextInt();
+    int alan=kisak*uzunk;
+        System.out.println("dikdörtgenin alanı :"+alan);*/
+   
+  /* int sayi1=10;
+   int sayi2=20;
+   int toplam=sayi1+sayi2;
+        System.out.println(toplam);
+   */
+ /* Scanner scan=new Scanner(System.in);
+        System.out.println("lütfen 1.sayıyı giriniz");
+   int sayi1=scan.nextInt();
+    System.out.println("lütfen 2.sayıyı giriniz");
+   int sayi2=scan.nextInt();
+   int toplam=sayi1+sayi2;
+System.out.println(toplam);*/
+   
+        
+        
+    }
+    
+}
